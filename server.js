9452a91b50cdbe0a3c085790677d9e7d0eca08d3
shubham1710 <shubhamstudent5@gmail.com>
2021-01-11 13:24:55 +0530

    Added config and server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const config = require('config');
+
+const app = express();
+app.use(express.json());
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
+
+const dbURI = config.get('dbURI');
+const port = process.env.PORT || 4000;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(port))
+  .catch((err) => console.log(err));

1ca70360991fea1c20f6ebfaf66866f4767d5463
shubham1710 <shubhamstudent5@gmail.com>
2021-01-11 15:39:11 +0530

    Added user.js

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema();
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    register_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = User = mongoose.model('user',UserSchema);

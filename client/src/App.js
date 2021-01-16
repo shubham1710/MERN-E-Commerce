d7b313094c33b71fc5d96c5808cd082462e0b94e
shubham1710 <shubhamstudent5@gmail.com>
2021-01-16 18:49:03 +0530

    Added load user

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,12 @@ import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar';
 import store from './store';
+import {loadUser} from './actions/authActions';
 
 class App extends Component {
-  
+  componentDidMount(){
+    store.dispatch(loadUser());
+  }
   render(){
     return ( 
       <Provider store={store}>

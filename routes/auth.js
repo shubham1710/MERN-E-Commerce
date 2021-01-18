f5d85a78cae3558a855e1dfa7008dcf3694c5ae8
shubham1710 <shubhamstudent5@gmail.com>
2021-01-18 08:19:32 +0530

    Changed server.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const authController = require('../controllers/authControllers');
 const router = Router();
+const auth = require('../middleware/auth');
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-router.get('/user', authController.get_user);
+router.get('/user', auth, authController.get_user);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,15 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const config = require('config');
+
 const authRoutes = require('./routes/auth');
 const itemRoutes = require('./routes/item');
 const cartRoutes = require('./routes/cart');
 const orderRoutes = require('./routes/order');
-const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(express.json());
-app.use(cookieParser());
+
+app.use('/api',authRoutes);
+app.use('/api',itemRoutes);
+app.use('/api',cartRoutes);
+app.use('/api',orderRoutes);
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
@@ -24,8 +28,3 @@ const port = process.env.PORT || 4000;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(port))
   .catch((err) => console.log(err));
-
-app.use('/api',authRoutes);
-app.use('/api',itemRoutes);
-app.use('/api',cartRoutes);
-app.use('/api',orderRoutes);

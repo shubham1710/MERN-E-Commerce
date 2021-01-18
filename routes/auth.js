c22e06cd2bb22d4b0cae75e58ac24d827a0b2aae
shubham1710 <shubhamstudent5@gmail.com>
2021-01-18 08:37:21 +0530

    fixed register route error

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const authController = require('../controllers/authControllers');
 const router = Router();
 const auth = require('../middleware/auth');
 
-router.post('/signup', authController.signup);
+router.post('/register', authController.signup);
 router.post('/login', authController.login);
 router.get('/user', auth, authController.get_user);
 
